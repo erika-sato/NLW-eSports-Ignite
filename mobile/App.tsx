89280cd6f41a1414a0fd82fb73d27b2a5236fa4b
erika sato <eerikasato@gmail.com>
2022-09-25 22:26:30 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,33 @@
+import { Background } from './src/components/Background';
+import { StatusBar } from 'react-native';
+import { Routes } from './src/routes';
+import { Loading } from './src/components/Loading';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+ } from '@expo-google-fonts/inter';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background >
+     <StatusBar
+     barStyle="light-content"
+     backgroundColor="transparent"
+     translucent
+     />
+     { fontsLoaded ? <Routes/> : <Loading/>}
+      
+    </Background>
+  );
+}
+
